fddc1c1d6058edc9f4f7952aedf31b809ed3d6ec
Brent <bbenofsky@gmail.com>
2020-04-11 14:36:42 +0800

    finish exercise 3.6

--- index.js
+++ index.js
@@ -58,8 +58,27 @@ app.delete('/api/persons/:id', (req, res) => {
 });
 
 app.post('/api/persons', (req, res) => {
-  const person = req.body;
-  person.id = Math.floor(Math.random() * 100000);
+  const body = req.body;
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: 'name and/or number missing',
+    });
+  }
+
+  const existing = persons.find((person) => person.name === body.name);
+
+  if (existing) {
+    return res.status(400).json({
+      error: 'name must be unique',
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: Math.floor(Math.random() * 100000),
+  };
+
   persons = persons.concat(person);
   res.json(person);
 });
