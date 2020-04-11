fbaca43488e95fe1dd5b3ef3e565dce1815e3a2b
Brent <bbenofsky@gmail.com>
2020-04-11 15:46:30 +0800

    finish exercise 3.8

--- index.js
+++ index.js
@@ -3,7 +3,19 @@ const morgan = require('morgan');
 const app = express();
 
 app.use(express.json());
-app.use(morgan('tiny'));
+
+morgan.token('person', function (req) {
+  if (req.body.name) {
+    return JSON.stringify(req.body);
+  }
+  return null;
+});
+
+app.use(
+  morgan(
+    ':method :url :status :res[content-length] - :response-time ms :person'
+  )
+);
 
 let persons = [
   {
