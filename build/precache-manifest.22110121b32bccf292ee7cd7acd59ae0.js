1862872196c46660facefc7d3bbd49c3d9a131cd
Brent <bbenofsky@gmail.com>
2020-04-11 17:08:31 +0800

    finish exercise 3.9

---
+++ build/precache-manifest.22110121b32bccf292ee7cd7acd59ae0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b4bf333d4abefb8617211925eb9702a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "376143b890f57d7f970e",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8c779c7d721b0ecb1447",
+    "url": "/static/js/2.673a006d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.673a006d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "376143b890f57d7f970e",
+    "url": "/static/js/main.60150d11.chunk.js"
+  },
+  {
+    "revision": "cbe135e57de162cd240f",
+    "url": "/static/js/runtime-main.d0b0a789.js"
+  }
+]);
